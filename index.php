b44397b924ba5d6ba0e0a48c3cd108f31c6d55b5
Adytm404 <41492762+Adytm404@users.noreply.github.com>
2020-11-25 18:31:41 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,257 @@
+
+<!DOCTYPE html>
+<html lang="en" class="link-html">
+    <head>
+        <title></title>
+        <base href="https://itsmy.id/">
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+        
+                    <link href="https://itsmy.id/uploads/favicon/d0371952682c8ddf8fe9d1f3007dd2ad.png" rel="shortcut icon" />
+        
+        
+                    <link href="https://itsmy.id/themes/altum/assets/css/bootstrap.min.css?v=741" rel="stylesheet" media="screen">
+                    <link href="https://itsmy.id/themes/altum/assets/css/custom.css?v=741" rel="stylesheet" media="screen">
+                    <link href="https://itsmy.id/themes/altum/assets/css/link-custom.css?v=741" rel="stylesheet" media="screen">
+                    <link href="https://itsmy.id/themes/altum/assets/css/animate.min.css?v=741" rel="stylesheet" media="screen">
+        
+                                <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+            <style>
+                body {
+                    font-family: 'Lato', sans-serif !important;
+                }
+            </style>
+        
+        
+        
+        <link rel="canonical" href="https://itsmy.id/PaymentSystem" />
+    </head>
+
+    
+<body class="link-body link-body-background-five" style="">
+    <div class="container animated fadeIn">
+        <div class="row d-flex justify-content-center text-center">
+            <div class="col-md-8 link-content ">
+
+                
+                <header class="d-flex flex-column align-items-center" style="color: #fff">
+                    <img id="image" src="https://itsmy.id/uploads/avatars/" alt="Avatar" class="link-image" style="display: none;" />
+
+                    <div class="d-flex flex-row align-items-center mt-4">
+                        <h1 id="title">Ogya Adyatma Putra </h1>
+
+                                            </div>
+
+                    <p id="description">Pilih salah satu</p>
+                </header>
+
+                <main id="links" class="mt-4">
+
+                                            
+                            
+                            <div data-link-id="56">
+                                
+<div class="my-3">
+    <h2 class="h4" style="color: #fff">Gopay</h2>
+    <p class="" style="color: #fff">085155313144</p>
+</div>
+
+                            </div>
+
+                        
+                            
+                            <div data-link-id="57">
+                                
+<div class="my-3">
+    <h2 class="h4" style="color: white">DANA</h2>
+    <p class="" style="color: white">085155313144</p>
+</div>
+
+                            </div>
+
+                        
+                            
+                            <div data-link-id="58">
+                                
+<div class="my-3">
+    <h2 class="h4" style="color: white">Link Aja</h2>
+    <p class="" style="color: white">085155313144</p>
+</div>
+
+                            </div>
+
+                        
+                            
+                            <div data-link-id="59">
+                                
+<div class="my-3">
+    <h2 class="h4" style="color: white">OVO</h2>
+    <p class="" style="color: white">085157337571</p>
+</div>
+
+                            </div>
+
+                        
+                            
+                            <div data-link-id="62">
+                                
+<div class="my-3">
+    <h2 class="h4" style="color: #fff">Bank BRI</h2>
+    <p class="" style="color: #fff">0177 0104 7628 503</p>
+</div>
+
+                            </div>
+
+                        
+                            
+                            <div data-link-id="60">
+                                
+<div class="my-3">
+    <a href="https://fb.com/adytm404.co.id" data-location-url="TUHL3oq2Gq" class="btn btn-block btn-primary link-btn link-btn-rounded animated infinite pulse delay-2s" style="background: #fff;color: #000">
+
+                    <i class="fab fa-facebook mr-1"></i>
+        
+        Facebook    </a>
+</div>
+
+
+                            </div>
+
+                        
+                            
+                            <div data-link-id="61">
+                                
+<div class="my-3">
+    <a href="https://Instagram.com/ogya_adytm" data-location-url="MgXcLXyv4C" class="btn btn-block btn-primary link-btn link-btn-rounded animated infinite pulse delay-2s" style="background: #fff;color: #000">
+
+                    <i class="fab fa-instagram mr-1"></i>
+        
+        Instagram    </a>
+</div>
+
+
+                            </div>
+
+                        
+                            
+                            <div data-link-id="64">
+                                
+<div class="my-3">
+    <a href="https://wa.me/6285842970192" data-location-url="gV4KeEODbJ" class="btn btn-block btn-primary link-btn link-btn-rounded animated infinite pulse delay-2s" style="background: #fff;color: #000">
+
+                    <i class="fab fa-whatsapp mr-1"></i>
+        
+        WhatsApp    </a>
+</div>
+
+
+                            </div>
+
+                                            
+                                        <div id="socials" class="d-flex flex-wrap justify-content-center mt-5">
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+                    </div>
+                    
+                </main>
+
+                
+                <footer class="link-footer">
+                                                                        <a id="branding" href="https://itsmy.id/" style="color: #fff">Payment System | by Ogya Adyatma</a>
+                                                            </footer>
+
+            </div>
+        </div>
+    </div>
+
+    </body>
+
+
+
+
+
+
+    <input type="hidden" id="url" name="url" value="https://itsmy.id/" />
+    <input type="hidden" name="global_token" value="93fb676f84c00eddb9415a2cfd24b425" />
+    <input type="hidden" name="number_decimal_point" value="." />
+    <input type="hidden" name="number_thousands_separator" value="," />
+
+            <script src="https://itsmy.id/themes/altum/assets/js/libraries/jquery.min.js?v=741"></script>
+            <script src="https://itsmy.id/themes/altum/assets/js/libraries/popper.min.js?v=741"></script>
+            <script src="https://itsmy.id/themes/altum/assets/js/libraries/bootstrap.min.js?v=741"></script>
+            <script src="https://itsmy.id/themes/altum/assets/js/main.js?v=741"></script>
+            <script src="https://itsmy.id/themes/altum/assets/js/functions.js?v=741"></script>
+            <script src="https://itsmy.id/themes/altum/assets/js/libraries/fontawesome.min.js?v=741"></script>
+    
+    <script>
+    /* Internal tracking for biolink links */
+    $('[data-location-url]').on('click', event => {
+
+        let base_url = $('[name="url"]').val();
+        let url = $(event.currentTarget).data('location-url');
+
+        $.ajax(`${base_url}${url}?no_redirect`);
+    });
+</script>
+<script>
+    /* Go over all mail buttons to make sure the user can still submit mail */
+    $('form[id^="mail_"]').each((index, element) => {
+        let link_id = $(element).find('input[name="link_id"]').val();
+        let is_converted = localStorage.getItem(`mail_${link_id}`);
+
+        if(is_converted) {
+            /* Set the submit button to disabled */
+            $(element).find('button[type="submit"]').attr('disabled', 'disabled');
+        }
+    });
+        /* Form handling for mail submissions if any */
+    $('form[id^="mail_"]').on('submit', event => {
+        let base_url = $('[name="url"]').val();
+        let link_id = $(event.currentTarget).find('input[name="link_id"]').val();
+        let is_converted = localStorage.getItem(`mail_${link_id}`);
+
+        if(!is_converted) {
+
+            $.ajax({
+                type: 'POST',
+                url: `${base_url}link-ajax`,
+                data: $(event.currentTarget).serialize(),
+                success: (data) => {
+                    let notification_container = $(event.currentTarget).find('.notification-container');
+
+                    if (data.status == 'error') {
+                        notification_container.html('');
+
+                        display_notifications(data.message, 'error', notification_container);
+                    } else if (data.status == 'success') {
+
+                        display_notifications(data.message, 'success', notification_container);
+
+                        setTimeout(() => {
+
+                            /* Hide modal */
+                            $(event.currentTarget).closest('.modal').modal('hide');
+
+                            /* Remove the notification */
+                            notification_container.html('');
+
+                            /* Set the localstorage to mention that the user was converted */
+                            localStorage.setItem(`mail_${link_id}`, true);
+
+                            /* Set the submit button to disabled */
+                            $(event.currentTarget).find('button[type="submit"]').attr('disabled', 'disabled');
+
+                        }, 1000);
+
+                    }
+                },
+                dataType: 'json'
+            });
+
+        }
+
+        event.preventDefault();
+    })
+</script>
+</html>
